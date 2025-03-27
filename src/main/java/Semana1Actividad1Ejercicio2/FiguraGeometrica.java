b089764f0bca782df9931c7c4a0ac04a368fa253
Josephtellolaiza <Joseph.tello@usil.pe>
2025-03-26 21:08:21 -0500

    primer commit

---
+++ src/main/java/Semana1Actividad1Ejercicio2/FiguraGeometrica.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Semana1Actividad1Ejercicio2;
+
+/**
+ *
+ * @author JOSEPH
+ */
+public abstract class FiguraGeometrica {
+    
+    public abstract double calcularArea();
+    public abstract boolean esRegular();
+
+    public abstract String getNombre();
+    
+}
